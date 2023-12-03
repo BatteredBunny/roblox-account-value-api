476a9fd283cec05b5623d40307746b79be3938fb
ayes-web <ayes2022@protonmail.com>
2023-12-03 04:39:40 +0200

    Bump deps

--- build.nix
+++ build.nix
@@ -1,11 +1,18 @@
-{ pkgs, buildGoModule, lib }: buildGoModule rec {
-    src = ./.;
+{buildGoModule}:
+buildGoModule rec {
+  src = ./.;
 
-    name = "roblox-account-value-api";
-    vendorSha256 = "sha256-pTw0KdOWjKDLb4rx87WGt9D6qok73J5uNRG+ecyAaaA=";
+  name = "roblox-account-value-api";
+  vendorHash = "sha256-++ocAIm+8xt7xYU5ZCVOaKt0zvROR0/jSb4623M6/d0=";
 
-    ldflags = [
-        "-s"
-        "-w"
-    ];
+  ldflags = [
+    "-s"
+    "-w"
+  ];
+
+  meta = {
+    homepage = "https://github.com/ayes-web/roblox-account-value-api";
+    description = "Webapp that calculates account value in robux and euro";
+    mainProgram = name;
+  };
 }
--- flake.nix
+++ flake.nix
@@ -1,35 +1,44 @@
 {
   inputs = {
-    nixpkgs.url       = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url   = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
     nix2container.url = "github:nlewo/nix2container";
   };
 
-  outputs = { self, nixpkgs, flake-utils, nix2container, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    nix2container,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
         pkgs = import nixpkgs {
-            inherit system;
+          inherit system;
         };
 
         # Needed on non linux systems since docker runs a linux vm
-        dockerCallPackage = if pkgs.stdenv.isLinux then
-            pkgs.callPackage
-        else
-            pkgs.pkgsCross."${pkgs.stdenv.hostPlatform.uname.processor}-multiplatform".callPackage;
+        dockerCallPackage =
+          if pkgs.stdenv.isLinux
+          then pkgs.callPackage
+          else pkgs.pkgsCross."${pkgs.stdenv.hostPlatform.uname.processor}-multiplatform".callPackage;
       in
-      with pkgs;
-      rec {
-        devShells.default = mkShell {
-          buildInputs = [
-            gnumake
-            wire
-            go
-          ];
-        };
+        with pkgs; {
+          devShells.default = mkShell {
+            buildInputs = [
+              gnumake
+              wire
+              go
+            ];
+          };
 
-        packages.default = callPackage ./build.nix { };
-        packages.docker = callPackage ./docker.nix { default = dockerCallPackage ./build.nix { }; inherit nix2container; };
-      }
+          packages = {
+            default = callPackage ./build.nix {};
+            docker = callPackage ./docker.nix {
+              default = dockerCallPackage ./build.nix {};
+              inherit nix2container;
+            };
+          };
+        }
     );
 }
