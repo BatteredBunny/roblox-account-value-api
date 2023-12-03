3ab60fe0b40efc0a8b5e0b709ed62cd1db73a3e3
BatteredBunny <ayes2022@protonmail.com>
2023-12-03 04:39:40 +0200

    Bump deps

--- docker.nix
+++ docker.nix
@@ -1,16 +1,22 @@
-{ pkgs, default, nix2container}: let
-    config = pkgs.writeText "config.toml" ''
-        port = "80"
-        robux_per_euro = 60
-    '';
-in nix2container.packages.${pkgs.system}.nix2container.buildImage {
+{
+  pkgs,
+  default,
+  nix2container,
+  lib,
+}: let
+  config = pkgs.writeText "config.toml" ''
+    port = "80"
+    robux_per_euro = 60
+  '';
+in
+  nix2container.packages.${pkgs.system}.nix2container.buildImage {
     name = "ghcr.io/ayes-web/roblox-account-value-api";
     tag = "latest";
 
     copyToRoot = pkgs.cacert;
 
     config = {
-        entrypoint = ["${default}/bin/roblox-account-value-api" "-c" "${config}"];
-        exposed_ports = [80];
+      entrypoint = ["${lib.getExe default}" "-c" "${config}"];
+      exposed_ports = [80];
     };
-}
+  }
